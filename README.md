6b21b184b283c769cf6d8d2b63c4d4c7dbfa9c4a
Akshay <akshay.ap95@gmail.com>
2022-05-29 22:06:30 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# inisghts-backend
+# insights-backend
+
+
+## Run mongodb
+
+```bash
+docker run --rm -it --name mongodb -p 27016:27017 -e MONGO_INITDB_ROOT_USERNAME=user -e MONGO_INITDB_ROOT_PASSWORD=user -v $PWD/mongodb_data:/data/db mongo
+```
+
+```bash
+pip install flask pymongo python-dotenv
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='insights-backend',
+   version='0.0.1',
+   description='A backend to manage forms',
+   author='Akshay Patel',
+   author_email='akshay.ap95@gmail.com',
+   packages=['insights-backend'],
+   install_requires=['Flask', 'pymongo', 'python-dotenv', 'bump2version'],
+)
